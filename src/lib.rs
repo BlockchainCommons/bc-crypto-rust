d39c2386ab31de753c471ede991000c5e7d2ba37
Wolf McNally <wolf@wolfmcnally.com>
2023-06-13 21:34:31 -0700

    Working.

--- src/lib.rs
+++ src/lib.rs
@@ -82,3 +82,16 @@ pub use seeded_random::{
 
 mod error;
 pub use error::Error;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_readme_deps() {
+        version_sync::assert_markdown_deps_updated!("README.md");
+    }
+
+    #[test]
+    fn test_html_root_url() {
+        version_sync::assert_html_root_url_updated!("src/lib.rs");
+    }
+}
