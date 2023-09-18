0ddd7338fa800b6460f4f452a2a828611620deac
Wolf McNally <wolf@wolfmcnally.com>
2023-09-18 03:01:12 -0700

    Using thiserror.

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,7 @@
-#[derive(Debug)]
+use thiserror::Error;
+
+#[derive(Debug, Error)]
 pub enum Error {
+    #[error("decrypt failed")]
     DecryptFailed
 }
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let s = match self {
-            Error::DecryptFailed => "Decrypt failed".to_string(),
-        };
-        f.write_str(&s)
-    }
-}
-
-impl std::error::Error for Error { }
